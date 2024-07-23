71aefd4e4af866461b25bafb9e3b8ba8eb7211ff
lukelike1001 <lsn12@duke.edu>
2024-07-23 15:30:51 -0400

    small joblib generated

--- expel/csv_to_joblib.py
+++ expel/csv_to_joblib.py
@@ -2,11 +2,11 @@ import pandas as pd
 import joblib
 
 # Step 1: Read the CSV file into a DataFrame
-csv_file_path = '/home/luke-skywalker/github/coa-model/expel/coa-query-sample.csv'
+csv_file_path = '/home/luke-skywalker/github/coa-model/expel/coa-query-small-sample.csv'
 df = pd.read_csv(csv_file_path)
 
 # Step 2: Save the DataFrame as a joblib object
-joblib_file_path = '/home/luke-skywalker/github/coa-model/expel/coa-query-sample.joblib'
+joblib_file_path = '/home/luke-skywalker/github/coa-model/expel/coa-query-small-sample.joblib'
 joblib.dump(df, joblib_file_path)
 
 print(f"CSV file has been successfully converted to a joblib object and saved as {joblib_file_path}")
